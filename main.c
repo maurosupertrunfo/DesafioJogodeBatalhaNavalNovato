24d4ab89d5546a7de46aae5fce0f53c0bfcc46e7
Mauro <maurosergiocaesar@gmail.com>
2025-10-30 16:57:20 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//Desafio: nível novato
+//Posicionando Navios no Tabuleiro
+//Desafio Jogo de Batalha Naval
+
+int main() {
+    int tabuleiro[10][10]; // declaracao do tabuleiro
+    int naviox[3];
+    int navioy[3];
+    
+    //iniciando a matrix-tabuleiro
+     for(int i=0; i<10; i++){
+        for (int j=0; j<10; j++){
+            tabuleiro[i][j]=0;
+            //printf(" %d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    
+    //inicializa navio com 3
+    for(int i=0; i<3; i++){
+        int naviox[i]=3;
+           
+    }
+        
+    
+    
+    return 0;
+}
